3367217fd5a9098005f4350ee669c74e189fad4b
Mathis Laroche <ecomath360@gmail.com>
2022-04-26 15:26:51 -0400

    Attemp 2 to fix the bridge

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include <WebSocketsClient.h>
-#include <ArduinoJson.h>
+//#include <WebSocketsClient.h>
+//#include <ArduinoJson.h>
 //#include <ESP8266WiFi.h>
 //#include <BlynkSimpleEsp8266.h>
 
@@ -11,6 +11,8 @@
 #include "stepper_driver/a4988.hpp"
 
 #include "debugging.hpp"
+#include "../lib/arduinoWebSockets-master/src/WebSocketsClient.h"
+#include "../lib/ArduinoJson/src/ArduinoJson.h"
 
 const char *ssid = "robot03";
 const char *password = "alive160";
@@ -34,10 +36,10 @@ const int stepsPerRevolution = 3200;
 
 // bridge.setOpenAngle sert à configurer l'angle d'ouverture. Augmenter si n'ouvre pas assez et diminuer si ouvre trop. Modifier le coefficient devant REVOLUTION_ANGLE dans la définition de REVOLUTION_ANGLE;
 MovableBridge<StepperDriver::A4988, StepperDriver::A4988> bridge(
-		StepperDriver::A4988::PinoutDescriptor(12, 14),
-		200,
-		StepperDriver::A4988::PinoutDescriptor(33, 32),
-		200);
+        StepperDriver::A4988::PinoutDescriptor(12, 14),
+        200,
+        StepperDriver::A4988::PinoutDescriptor(33, 32),
+        200);
 
 WebSocketsClient webSocket;
 // WiFiClient client;
@@ -64,196 +66,202 @@ bool readyToGo = false;
  *
  * @param eventName
  */
-void _sendEvent(const char *eventName, StaticJsonDocument<256> responseData)
+void _sendEvent(const char *eventName, const StaticJsonDocument<256>& responseData)
 {
-	StaticJsonDocument<256> doc;
-	doc["event"] = eventName;
-	doc["data"] = responseData;
-	String eventSerialized;
-	serializeJson(doc, eventSerialized);
-	doc.clear();
-	webSocket.sendTXT(eventSerialized);
+    StaticJsonDocument<256> doc;
+    doc["event"] = eventName;
+    doc["data"] = responseData;
+    String eventSerialized;
+    serializeJson(doc, eventSerialized);
+    doc.clear();
+    webSocket.sendTXT(eventSerialized);
 }
 
 void wait(uint16_t waitTime)
 {
-	uint16_t maxTimeBetweenUpdates = 2000;
+    uint16_t maxTimeBetweenUpdates = 2000;
 
-	if (waitTime <= maxTimeBetweenUpdates)
-	{
-		Serial.println("Minimum treshold not reached");
-		Serial.println(waitTime);
-		delay(waitTime);
-	}
-	else
-	{
-		uint8_t nbUpdates = waitTime / maxTimeBetweenUpdates + 1;
-		Serial.println("Number of iterations");
-		Serial.println(nbUpdates);
-		for (uint8_t i = 0; i < nbUpdates; i++)
-		{
-			if (i + 1 == nbUpdates)
-			{
-				Serial.println(waitTime - (uint16_t)(nbUpdates - 1) * maxTimeBetweenUpdates);
-				delay(waitTime - (uint16_t)(nbUpdates - 1) * maxTimeBetweenUpdates);
-			}
-			else
-			{
-				Serial.println(maxTimeBetweenUpdates);
-				delay(maxTimeBetweenUpdates);
-			}
-			webSocket.loop();
-		}
-	}
+    if (waitTime <= maxTimeBetweenUpdates)
+    {
+        Serial.println("Minimum treshold not reached");
+        Serial.println(waitTime);
+        delay(waitTime);
+    } else
+    {
+        uint8_t nbUpdates = waitTime / maxTimeBetweenUpdates + 1;
+        Serial.println("Number of iterations");
+        Serial.println(nbUpdates);
+        for (uint8_t i = 0; i < nbUpdates; i++)
+        {
+            if (i + 1 == nbUpdates)
+            {
+                Serial.println(waitTime - (uint16_t) (nbUpdates - 1) * maxTimeBetweenUpdates);
+                delay(waitTime - (uint16_t) (nbUpdates - 1) * maxTimeBetweenUpdates);
+            } else
+            {
+                Serial.println(maxTimeBetweenUpdates);
+                delay(maxTimeBetweenUpdates);
+            }
+            webSocket.loop();
+        }
+    }
 }
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 {
-	switch (type)
-	{
-	case WStype_DISCONNECTED:
-		Serial.printf("[WSc] Disconnected!\n");
-		break;
-	case WStype_CONNECTED:
-	{
-		Serial.printf("[WSc] Connected to url: %s\n", payload);
-		webSocket.sendTXT("{\"event\": \"connect_object\", \"data\": { \"id\": \"27d8ec40-4504-459b-98d7-d32a36d92d5f\"}}");
-		break;
-	}
-	case WStype_TEXT:
-	{
-		StaticJsonDocument<256> doc;
-		Serial.printf("[WSc] get text: %s\n", payload);
-		doc.clear();
-		deserializeJson(doc, (char *)payload);
-		String event = String(doc["event"].as<const char *>());
+    switch (type)
+    {
+        case WStype_DISCONNECTED:
+            Serial.printf("[WSc] Disconnected!\n");
+            break;
+        case WStype_CONNECTED:
+        {
+            Serial.printf("[WSc] Connected to url: %s\n", payload);
+            webSocket.sendTXT(
+                    R"({"event": "connect_object", "data": { "id": "27d8ec40-4504-459b-98d7-d32a36d92d5f"}})");
+            break;
+        }
+        case WStype_TEXT:
+        {
+            StaticJsonDocument<256> doc;
+            Serial.printf("[WSc] get text: %s\n", payload);
+            doc.clear();
+            deserializeJson(doc, (char *) payload);
+            String event = String(doc["event"].as<const char *>());
 
-		// ping
-		if (event == EVT_PING || event == "")
-		{
-			webSocket.sendTXT(R"({"event": "pong"})");
-			break;
-		}
+            // ping
+            if (event == EVT_PING || event == "")
+            {
+                webSocket.sendTXT(R"({"event": "pong"})");
+                break;
+            }
 
-		if (event == EVT_CONNECT_SUCCESS && !readyToGo)
-		{
-			readyToGo = true;
-			// TODO register listeners
-			Serial.println("Connected to ALIVEcode");
-			// voiture.forward(100);
-			break;
-		}
-		else if (event == EVT_ERROR)
-		{
-			// TODO handle errors
-			Serial.println("Error received '");
-			Serial.print(event);
-			Serial.print("': ");
-			Serial.print(doc["data"].as<const char *>());
-			break;
-		}
+            if (event == EVT_CONNECT_SUCCESS && !readyToGo)
+            {
+                readyToGo = true;
+                // TODO register listeners
+                Serial.println("Connected to ALIVEcode");
+                // voiture.forward(100);
+                break;
+            } else if (event == EVT_ERROR)
+            {
+                // TODO handle errors
+                Serial.println("Error received '");
+                Serial.print(event);
+                Serial.print("': ");
+                Serial.print(doc["data"].as<const char *>());
+                break;
+            }
 
-		if (!readyToGo)
-		{
-			break;
-		}
+            if (!readyToGo)
+            {
+                break;
+            }
 
-		Serial.println(event);
-		// TODO handle callbacks
-		if (event == EVT_ON_RECV)
-		{
-			byte actionId = doc["data"]["id"].as<byte>();
-			switch (actionId)
-			{
-			case 0:
-				bridge.close();
-				webSocket.sendTXT("{\"event\": \"update_doc\",\"data\": {\"fields\": {\"/document/pont/ouvert\": false}}}");
-				break;
-			case 1:
-				webSocket.sendTXT("{\"event\": \"update_doc\",\"data\": {\"fields\": {\"/document/pont/ouvert\": true}}}");
-				bridge.open();
-				break;
-			}
-			Serial.println("DONE");
-			StaticJsonDocument<256> responseData;
-			responseData["actionId"] = actionId;
-			responseData["value"] = NULL;
-			_sendEvent(EVT_ACTION_DONE, responseData);
-			responseData.clear();
-			doc.clear();
+            Serial.println(event);
+            // TODO handle callbacks
+            if (event == EVT_ON_RECV)
+            {
+                byte actionId = doc["data"]["id"];
+                switch (actionId)
+                {
+                    case 0:
+                        bridge.close();
+                        webSocket.sendTXT(
+                                R"({"event": "update_doc","data": {"fields": {"/document/pont/ouvert": false}}})");
+                        break;
+                    case 1:
+                        webSocket.sendTXT(
+                                R"({"event": "update_doc","data": {"fields": {"/document/pont/ouvert": true}}})");
+                        bridge.open();
+                        break;
+                    default:
+                        break;
+                }
+                Serial.println("DONE");
+                StaticJsonDocument<256> responseData;
+                responseData["actionId"] = actionId;
+                responseData["value"] = NULL;
+                _sendEvent(EVT_ACTION_DONE, responseData);
+                responseData.clear();
+                doc.clear();
 
-			webSocket.loop();
-		}
-		break;
-	}
-	case WStype_BIN:
-		Serial.printf("[WSc] get binary length: %u\n", length);
-		// hexdump(payload, length);
+                webSocket.loop();
+            }
+            break;
+        }
+        case WStype_BIN:
+            Serial.printf("[WSc] get binary length: %zu\n", length);
+            // hexdump(payload, length);
 
-		// send data to server
-		// webSocketTXT.sendBIN(payload, length);
-		break;
-	case WStype_ERROR:
-	case WStype_FRAGMENT_TEXT_START:
-	case WStype_FRAGMENT_BIN_START:
-	case WStype_FRAGMENT:
-	case WStype_FRAGMENT_FIN:
-		break;
-	}
+            // send data to server
+            // webSocketTXT.sendBIN(payload, length);
+            break;
+        case WStype_ERROR:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:
+            break;
+        case WStype_PING:
+            break;
+        case WStype_PONG:
+            break;
+    }
 }
 
 void setup()
 {
-	debugInit();
+    debugInit();
 
-	WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-	uint32_t notConnectedCounter = 0;
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		delay(1000);
-		Serial.print(".");
-		notConnectedCounter++;
-		if (notConnectedCounter > 10)
-		{ // Reset board if not connected after 10s
-			Serial.println("Resetting due to Wifi not connecting...");
-			ESP.restart();
-		}
-	}
-	Serial.println("Connection au reseau reussie!");
+    uint32_t notConnectedCounter = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+        notConnectedCounter++;
+        if (notConnectedCounter > 10)
+        { // Reset board if not connected after 10s
+            Serial.println("Resetting due to Wifi not connecting...");
+            ESP.restart();
+        }
+    }
+    Serial.println("Connection au reseau reussie!");
 
-	pinMode(stepPin, OUTPUT);
-	pinMode(dirPin, OUTPUT);
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
 
-	pinMode(stepPin2, OUTPUT);
-	pinMode(dirPin2, OUTPUT);
+    pinMode(stepPin2, OUTPUT);
+    pinMode(dirPin2, OUTPUT);
 
-	// initialize the switch pin as an input:
-	pinMode(sensA.getPinE(), INPUT);
-	pinMode(sensA.getPinS(), INPUT);
-	pinMode(sensB.getPinE(), INPUT);
-	pinMode(sensB.getPinS(), INPUT);
-	// event handler
-	webSocket.onEvent(webSocketEvent);
+    // initialize the switch pin as an input:
+    pinMode(sensA.getPinE(), INPUT);
+    pinMode(sensA.getPinS(), INPUT);
+    pinMode(sensB.getPinE(), INPUT);
+    pinMode(sensB.getPinS(), INPUT);
+    // event handler
+    webSocket.onEvent(webSocketEvent);
 
-	// server address, port and URL
-	webSocket.begin(host, port, path);
+    // server address, port and URL
+    webSocket.begin(host, port, path);
 
-	// try every 7000 again if connection has failed
-	webSocket.setReconnectInterval(7000);
+    // try every 7000 again if connection has failed
+    webSocket.setReconnectInterval(7000);
 
-	// Objet pont levant
-	bridge.setOpenAngle(10 * 2 * PI);
+    // Objet pont levant
+    bridge.setOpenAngle(10 * 2 * PI);
 }
 
 void loop()
 {
-	// --- Switch ---
-	// Add or substract a car to the bridge
-	sensA.change(digitalRead(sensA.getPinE()), digitalRead(sensA.getPinS()));
-	sensB.change(digitalRead(sensB.getPinE()), digitalRead(sensB.getPinS()));
-	// Serial.println(digitalRead(15));
-	// debugPrintln(digitalRead(sensA.getPinE()));
+    // --- Switch ---
+    // Add or substract a car to the bridge
+    sensA.change(digitalRead(sensA.getPinE()), digitalRead(sensA.getPinS()));
+    sensB.change(digitalRead(sensB.getPinE()), digitalRead(sensB.getPinS()));
+    // Serial.println(digitalRead(15));
+    // debugPrintln(digitalRead(sensA.getPinE()));
 
-	webSocket.loop();
+    webSocket.loop();
 }
